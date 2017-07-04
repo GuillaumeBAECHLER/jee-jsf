30c26280aaa4ba2c8309b66e2481213f003e495f
GuillaumeBAECHLER <baechler.guillaume.pro@gmail.com>
2017-07-04 16:12:29 +0200

    3.2. managed beans, navigation and counter controller

---
+++ src/java/monapp/Counter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monapp;
+
+import java.io.Serializable;
+import javax.annotation.ManagedBean;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+
+/**
+ *
+ * @author Guillaume
+ */
+@Named(value = "counter")
+@ManagedBean
+@SessionScoped
+public class Counter implements Serializable {
+    Integer value = 1000;
+    
+    /**
+     * Creates a new instance of Counter
+     */
+    public Counter() {
+    }
+    
+    public String inc() {
+        value++;
+        return null; // ne pas se déplacer
+    }
+    
+    public Integer getValue() {
+        return value;
+    }
+    
+}
---
+++ src/java/monapp/Navigation.java
@@ -0,0 +1,32 @@
+package monapp;
+
+
+import java.io.Serializable;
+import javax.annotation.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Guillaume
+ */
+@Named(value = "navigation")
+@ManagedBean
+@SessionScoped
+public class Navigation implements Serializable {
+
+    /**
+     * Creates a new instance of Navigation
+     */
+    public Navigation() {
+    }
+
+    public String hello() {
+        return "hello";
+    }
+}
